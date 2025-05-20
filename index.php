5867b864421dbaf2d15a1d98b1464a240fe931e4
Joshua Davies <joshuadav808@gmail.com>
2025-05-21 09:28:00 +1200

    Add template files

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="Creative Commons Showcase"> 
+<meta name="keywords" content="Creative Commons, images, gallery">
+<meta name="author" content="Miss Gottschalk">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+<title>Creative Commons Showcase</title>
+
+<!-- Edit the link below / replace with your chosen google font -->
+<link href="https://fonts.googleapis.com/css?family Lato%7cUbuntu" rel="stylesheet">
+
+
+<!-- Edit the name of your style sheet 'foo' is not a valid name!! -->
+<link rel="stylesheet" href="layout.css">
+</head>
+
+<body>
+
+<div class="wrapper">
+<div class="banner"> Banner goes here
+</div>
+<div class="box nav">
+Navigation goes here
+</div>
+<div class="box main">
+Main content goes here
+</div>
+<div class="box footer">
+CC yourname 20XX
+</div> <!--/footer -->
+
+</body>
+</html> 
