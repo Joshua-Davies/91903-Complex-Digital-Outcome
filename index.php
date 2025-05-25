636ff75c6d7b7527369a6b6e642f1b8fb934f4fc
Skuffy <joshuadav808@gmail.com>
2025-05-25 22:46:13 +1200

    Add Homepage Content

--- index.php
+++ index.php
@@ -59,7 +59,16 @@
     </div>
 </div>
 <div class="box main">
-Main content goes here
+    <div class="info-text">
+        <h1>Home</h1>
+        <p>Welcome to our website dedicated to the preservation of the world's forests. Join us in our mission to protect these vital ecosystems.</p>
+        <ul class="text">
+            <li>Learn about the importance of forests</li>
+            <li>Discover ways to get involved</li>
+            <li>Support our conservation efforts</li>
+            <li>Explore our gallery of stunning forest images</li>
+        </ul>
+    </div>
 </div>
 <div class="box footer">
 CC Joshua Davies 20XX
