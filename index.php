43f02ba232129286684a61758aad6933e05c1951
Skuffy <skuffy808@gmail.com>
2025-05-25 21:46:52 +1200

    Add Navigation

--- index.php
+++ index.php
@@ -3,19 +3,21 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<meta name="description" content="Creative Commons Showcase"> 
-<meta name="keywords" content="Creative Commons, images, gallery">
-<meta name="author" content="Miss Gottschalk">
+<meta name="description" content="A website dedicated to the preservation of the world's forests."> 
+<meta name="keywords" content="forests, conservation, environment, save the canopy">
+<meta name="author" content="Joshua Davies">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-<title>Creative Commons Showcase</title>
+<title>Save the Canopy</title>
 
 <!-- Edit the link below / replace with your chosen google font -->
 <link href="https://fonts.googleapis.com/css?family Lato%7cUbuntu" rel="stylesheet">
 
 
 <!-- Edit the name of your style sheet 'foo' is not a valid name!! -->
-<link rel="stylesheet" href="layout.css">
+<link rel="stylesheet" href="css/layout.css">
+<link rel="stylesheet" href="css/navigation.css">
+
 </head>
 
 <body>
@@ -24,13 +26,24 @@
 <div class="banner"> Banner goes here
 </div>
 <div class="box nav">
-Navigation goes here
+    <div class="menu-header">
+        <i class="fa fa-bars"></i>
+        <div class="menu-content">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Gallery</a>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Links</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
 <div class="box main">
 Main content goes here
 </div>
 <div class="box footer">
-CC yourname 20XX
+CC Joshua Davies 20XX
 </div> <!--/footer -->
 
 </body>
